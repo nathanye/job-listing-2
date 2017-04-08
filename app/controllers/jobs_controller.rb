6cc669c4cabedd9e2549b74c444965c9cecbd102
nathanye <khalil54781@gmail.com>
2017-04-08 16:24:47 +0800

    create navbar Adming Panel

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,10 @@
 class JobsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+
+
   def index
     @jobs = Job.all
+    @jobs = @jobs.order("created_at DESC")
   end
 
   def show
