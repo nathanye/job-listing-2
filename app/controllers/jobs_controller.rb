c230d0c4f24834232251bfc594a1a4fcd6dcc59b
nathanye <khalil54781@gmail.com>
2017-04-08 15:26:43 +0800

    implement jobs#show

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,15 @@
+class JobsController < ApplicationController
+  def index
+    @jobs = Job.all
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+end
